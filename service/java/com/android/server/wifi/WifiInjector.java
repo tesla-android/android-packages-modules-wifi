142c21c95a316bcb167ccbc74d4bf702749d0902
William Escande <wescande@google.com>
2021-09-03 17:02:38 +0200

    Migrate dependency from base to modules-utils
    Use stand-alone modules-utils library.
    
    Bug: 198418216
    Tag: #refactor
    Test: atest FrameworksWifiApiTests
    Change-Id: Ieaf6ae5aafd8c61207f83c0347346a98e13145d2

--- service/java/com/android/server/wifi/WifiInjector.java
+++ service/java/com/android/server/wifi/WifiInjector.java
@@ -34,7 +34,6 @@ import android.net.wifi.WifiScanner;
 import android.net.wifi.nl80211.WifiNl80211Manager;
 import android.os.BatteryStatsManager;
 import android.os.Handler;
-import android.os.HandlerExecutor;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Process;
@@ -49,6 +48,7 @@ import android.util.LocalLog;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.modules.utils.HandlerExecutor;
 import com.android.modules.utils.build.SdkLevel;
 import com.android.server.wifi.aware.WifiAwareMetrics;
 import com.android.server.wifi.coex.CoexManager;
