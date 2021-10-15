66d47abd54931b437d524f9f98b8ddef92acd9c9
Nate Jiang <qiangjiang@google.com>
2021-10-15 16:54:10 -0700

    Only call addVcnNetworkPolicyChangeListener on first time
    On register the listner first time entering L2ConnectedState to avoid
    Binder leak.
    
    Bug: 203158714
    Test: atest ClientModeImplTest
    Change-Id: I4b68c35fe215ead43d23b413c67797629416af0a

--- service/tests/wifitests/src/com/android/server/wifi/ClientModeImplTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/ClientModeImplTest.java
@@ -6451,7 +6451,7 @@ public class ClientModeImplTest extends WifiBaseTest {
             });
 
         // Verify VCN policy listener is registered
-        verify(vcnManager, atLeastOnce()).addVcnNetworkPolicyChangeListener(any(),
+        verify(vcnManager).addVcnNetworkPolicyChangeListener(any(),
                 policyChangeListenerCaptor.capture());
         assertNotNull(policyChangeListenerCaptor.getValue());
 
