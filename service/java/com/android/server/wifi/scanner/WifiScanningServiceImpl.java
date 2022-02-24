06c0f5fddc9350669ab63665faf4eac441327bca
Chris Ye <lzye@google.com>
2022-02-24 09:15:37 -0800

    Check null pointer returned from removePnoScanRequest
    removePnoScanRequest returns the removed PNO scan request and if the
    request is not in active PNO scan request list it may return null.
    
    Bug: 218900974
    Test: atest FameworksWifiTests
    Change-Id: Iab1ae78260afe4050a1cba3a9758c95b26225804

--- service/java/com/android/server/wifi/scanner/WifiScanningServiceImpl.java
+++ service/java/com/android/server/wifi/scanner/WifiScanningServiceImpl.java
@@ -2336,8 +2336,10 @@ public class WifiScanningServiceImpl extends IWifiScanner.Stub {
         private void removeHwPnoScanRequest(ClientInfo ci, int handler) {
             if (ci != null) {
                 Pair<PnoSettings, ScanSettings> settings = removePnoScanRequest(ci, handler);
-                logScanRequest("removeHwPnoScanRequest", ci, handler, null,
-                        settings.second, settings.first);
+                if (settings != null) {
+                    logScanRequest("removeHwPnoScanRequest", ci, handler, null,
+                            settings.second, settings.first);
+                }
             }
         }
 
